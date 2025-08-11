7b13e3011ea5602aebdce48a18f69768244a49cd
x.karimjonov <khtkarimzhonov@gmail.com>
2025-08-11 10:19:37 +0500

    add prod script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Настройки сервера
+SERVER_USER="root"
+SERVER_HOST="ser-mebel.uz"
+SSH_KEY="~/.ssh/id_rsa"  # путь к приватному ключу
+
+# Команда для деплоя
+DEPLOY_COMMANDS=$(cat << 'ENDSSH'
+cd /var/www/ser-mebel.uz || exit
+git pull origin master
+docker-compose down
+docker build -t khtkarimzhonov/api.ser-mebel.uz:latest ./backend
+docker-compose -f docker-compose.prod.yml up -d
+ENDSSH
+)
+
+# Подключение и выполнение команд
+ssh -i $SSH_KEY -o StrictHostKeyChecking=no ${SERVER_USER}@${SERVER_HOST} "$DEPLOY_COMMANDS"
