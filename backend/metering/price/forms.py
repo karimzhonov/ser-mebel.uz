aa529993f3e6ea145bb397f8b401bc615617443f
Khusniddin <khtkarimzhonov@gmail.com>
2025-10-25 11:39:09 +0500

    fixes

--- backend/metering/price/forms.py
+++ backend/metering/price/forms.py
@@ -1,7 +1,7 @@
 import json
 from django import forms
 from djmoney.money import Money
-from unfold.widgets import UnfoldAdminSelectWidget, UnfoldAdminDecimalFieldWidget, AdminTextInputWidget
+from unfold.widgets import UnfoldAdminSelectWidget, UnfoldAdminDecimalFieldWidget
 from .models import Calculate, InventoryInCalculate, Inventory, InventoryType
 
 
@@ -64,7 +64,7 @@ class InventoryCountWidget(forms.MultiWidget):
         widgets = [
             UnfoldAdminSelectWidget(choices=choices),
             UnfoldAdminDecimalFieldWidget(attrs={'placeholder': 'Количество'}),
-            AdminTextInputWidget(attrs={'disabled': True}),
+            UnfoldAdminDecimalFieldWidget(attrs={'disabled': True}),
         ]
         super().__init__(widgets, attrs)
 
