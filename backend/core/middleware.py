a36e32a5a1727931e54ee155957ad0fa730ff707
Xusniddin Karimjonov <karimjonov.xusniddin@loc.tashkent.uz>
2026-01-10 14:59:24 +0500

    fixes

--- backend/core/middleware.py
+++ backend/core/middleware.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect
+from django.utils.deprecation import MiddlewareMixin
 from django.http import Http404
 
-class Redirect404Middleware:
+class Redirect404Middleware(MiddlewareMixin):
     def process_exception(self, request, exception):
         if isinstance(exception, Http404):
             return redirect("/admin/")
