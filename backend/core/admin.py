125dcc0d8d1ae18a8dcea65e782bf10e424cfe57
Xusniddin Karimjonov <karimjonov.xusniddin@loc.tashkent.uz>
2026-02-09 17:57:42 +0500

    fixes

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,4 @@
+import os
 import mimetypes
 from urllib.parse import quote
 from django.contrib import admin
@@ -24,51 +25,42 @@ class UFolderAdmin(FolderAdmin, ModelAdmin):
 
 
 @admin.register(File)
-class UFileAdmin(FileAdmin, ModelAdmin):
-    fieldsets = ()
-    fields = ['preview', 'owner', 'uploaded_at']
+class UFileAdmin(FileAdmin):
     readonly_fields = ['preview', 'owner', 'uploaded_at']
-    inlines = [DiscussionInline]
 
-    def get_model_perms(self, request):
-        """
-        It seems this is only used for the list view. NICE :-)
-        """
-        return {
-            'add': False,
-            'change': False,
-            'delete': True,
-        }
-    
-    def has_delete_permission(self, request, obj=None):
-        return request.user.is_superuser
-    
-    @display(description='Файл')
+    @admin.display(description='Файл')
     def preview(self, obj: File):
-        return format_html(f'<a href="{obj.file.url}" target="_blank">{obj.original_filename}</a>') if obj.canonical_url else '-'
+        return format_html(
+            '<a href="{}" target="_blank">Открыть файл</a>',
+            f'preview/{obj.pk}/'
+        )
 
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
             path(
-                '<path:object_id>/change/',
-                self.admin_site.admin_view(self.redirect_to_file),
+                'preview/<int:file_id>/',
+                self.admin_site.admin_view(self.preview_file),
+                name='filer_preview',
             ),
         ]
         return custom_urls + urls
 
-    def redirect_to_file(self, request, object_id):
-        obj: File = self.get_object(request, object_id)
-        file_handle = obj.file.open()
+    def preview_file(self, request, file_id):
+        obj = File.objects.get(pk=file_id)
 
         content_type, _ = mimetypes.guess_type(obj.file.name)
 
         response = FileResponse(
-            file_handle,
+            obj.file.open('rb'),
             content_type=content_type or 'application/octet-stream'
         )
 
-        response['Content-Disposition'] = "inline; filename*=UTF-8''{}".format(quote(obj.file.name))
+        filename = os.path.basename(obj.file.name)
+
+        response['Content-Disposition'] = (
+            "inline; filename*=UTF-8''{}".format(quote(filename))
+        )
 
         return response
 
