2dbb895aad77a1d0eb2b7ae93188387b019033f4
Khusniddin <khtkarimzhonov@gmail.com>
2026-03-25 09:37:21 +0500

    fixes

---
+++ backend/order/assembly/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.db.models import Q
+from unfold.widgets import UnfoldAdminSelectWidget
+from oauth.models import User
+from oauth.constants import ASSEMBLY_PERMISSION
+from .models import Assembly
+
+
+class AssemblyForm(forms.ModelForm):
+    user = forms.ModelChoiceField(
+        widget=UnfoldAdminSelectWidget()
+    )
+
+    class Meta:
+        model = Assembly
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields["user"].queryset = User.objects.filter(Q(user_permissions__codename=ASSEMBLY_PERMISSION) | Q(groups__permissions__codename=ASSEMBLY_PERMISSION))
